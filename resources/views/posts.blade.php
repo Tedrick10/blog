aeafc0b5ec64a602e58a59ef91cc99bcdf18373c
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2023-02-13 10:07:09 +0630

    First commit.

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+@extends('./components/layout')
+
+@section('content')
+    @foreach ($posts as $post)
+    <article>
+        <h1>
+            <a href="/posts/{{ $post -> id }}">
+                <?= $post -> title ?>
+            </a>
+        </h1>
+        <div>{{ $post -> excerpt }}</div>
+    </article>
+    @endforeach
+@endsection
